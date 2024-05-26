f3dbf8f170f45c0a45c80676e14423c965a14ad8
Kapil Kumawat <kapilkumawat966@gmail.com>
2024-05-26 19:55:19 +0530

    03_conversionOperation.js

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -18,10 +18,18 @@ let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
 // "" => false
-// "hitesh" => true
+// "kapil" => true
+
+
+let someNumber  = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
    
